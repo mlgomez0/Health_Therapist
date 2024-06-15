71daa57fe260edd52178796f5ea7cf88806bddb2
Jesus Andres Correal Ortiz <jcorreal@ioip.com.co>
2024-06-15 13:47:51 -0400

    Class Rag.py was created to allow configure the behavior of the API based on the selected model

---
+++ backend/api/src/Rag.py
@@ -0,0 +1,8 @@
+
+class Rag():
+
+    def __init__(self) -> None:
+        pass
+        
+    def predict(self, user_input: str) -> str:
+        return user_input
--- backend/api/src/request.py
+++ backend/api/src/request.py
@@ -2,3 +2,4 @@ from pydantic import BaseModel
 
 class Request(BaseModel):
     text: str
+    model: str
